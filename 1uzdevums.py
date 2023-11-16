99555c566572e89232e4e504191ad78589667631
tomslaizans <toms.laizans@grobinasskola.lv>
2023-11-16 14:19:37 +0200

    1. uzdevums par skaitīšanu

---
+++ 1uzdevums.py
@@ -0,0 +1,11 @@
+#lietotaja cipars
+cipars=int(input("Ievadi ciparu, kurš ir vismaz 1:"))
+
+#parbauda vai ir vismaz 1
+while cipars < 0:
+    print("Ievieto ciparu, kas ir vismaz 1")
+    cipars=int(input("Tagad ievadi ciparu, kurš ir vismaz 1:"))
+
+#Skaita
+for vismaz_viens in range(1, cipars+1):
+    print(vismaz_viens)
