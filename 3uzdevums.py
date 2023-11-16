39cf508079e58558d572ff7d8e71a0ce57f22511
tomslaizans <toms.laizans@grobinasskola.lv>
2023-11-16 14:44:44 +0200

    3. uzdevums

---
+++ 3uzdevums.py
@@ -0,0 +1,7 @@
+#Ievadit ciparu
+cipars=int(input("Ievadi ciparu:"))
+#parbaudit vai neapāra
+if cipars%2==0:
+    print(f"{cipars} ir pāra skaitlis")
+else:
+    print(f"{cipars} ir nepāra skaitlis")
